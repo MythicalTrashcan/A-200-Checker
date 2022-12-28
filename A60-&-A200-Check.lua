78c7ba6cd60e0e1ea1d4f25c90a7446d93d36562
MythicalTrashcan <79861302+MythicalTrashcan@users.noreply.github.com>
2022-12-27 21:51:04 -0500

    Update A60-&-A200-Check.lua

--- A60-&-A200-Check.lua
+++ A60-&-A200-Check.lua
@@ -1,3 +1,3 @@
 -- THIS SCRIPT ALSO NOTIFIES WHEN A-60 SPAWNS. DO NOT USE THIS SCRIPT IF YOU ONLY WANT A-200 NOTIFICATION
 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/MythicalTrashcan/A-200-Checker/main/Raw%20Code/Entity-Checker.lua')))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/MythicalTrashcan/A-200-Checker/main/Raw%20Code/A60-%26-A200-Check.lua')))()
